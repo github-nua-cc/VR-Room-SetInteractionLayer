6bfae2e2c1f8ebc8991160a2387500d8ba3f1686
github-nua-cc <113679327+github-nua-cc@users.noreply.github.com>
2023-02-15 20:00:55 +0000

    Add files via upload

---
+++ SetInteractionLayer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+/// <summary>
+/// Set the interaction layer of an interactor
+/// </summary>
+public class SetInteractionLayer : MonoBehaviour
+{
+
+    private XRBaseInteractor interactor = null;
+    private InteractionLayerMask originalLayer;
+    [Tooltip("The layer or layers that are switched to")]
+    public InteractionLayerMask targetLayer = 0;
+
+
+    private void Awake()
+    {
+        interactor = GetComponent<XRBaseInteractor>();
+        originalLayer = interactor.interactionLayers;
+    }
+
+    public void ToggleTargetLayer()
+    {
+        if(interactor != null)
+        {
+            if (interactor.interactionLayers != originalLayer)
+            {
+
+                interactor.interactionLayers = originalLayer;
+            }
+            else
+            {
+                interactor.interactionLayers = targetLayer;
+            }
+        }
+
+    }
+
+}
